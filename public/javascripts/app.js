7024c06ba69d899fa445677bbdcd72c8acf4da62
Administrator <rabih.elzahr@icloud.com>
2015-09-25 15:36:26 -0400

    Implemented Solution

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,5 +2,49 @@
 
     'use strict';
 
-    var app = angular.module("app", []);
+    var app = angular.module("app", [
+    	'osdForm',
+    	'ngLodash'
+    ])
+    .service('authenticationService', function( $http, $q ) {
+	   
+	    function authenticate(userInfo){
+	    	var req = $http({method:"post", url:"/login", data: userInfo})
+	    	return (req.then(handleSuccess,handleError));
+	    }
+
+	    function handleSuccess( response ) {
+	        return response.data ;
+	    }
+
+	    function handleError( response ) {
+	    	// If the response does contain error message, use expected error message 
+	        if (angular.isObject(response.data) && 'undefined' !== typeof response.data.message) {
+	        	// Otherwise, use default error message
+	        	return( $q.reject( response.data.message ) );
+	        }
+	        else {
+            	return( $q.reject( "An unknown error occurred." ) );
+	        }
+	    }
+
+	    return({
+	        login: authenticate,
+	    });
+	})
+
+	.controller('loginCtrl', ['authenticationService', '$scope', 
+		function(authenticationService, $scope){
+			$scope.data = {};
+
+			$scope.submit = function() {
+				authenticationService.login($scope.data).then(function(res){
+					console.log('Login success',res);
+				})
+				.catch(function (err) {
+					console.log('Login failed',err);
+				}); 
+			}
+		}
+	]);
 })();
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Osedea' });
 });
 
+/* POST authentication request */
+router.post('/login', function(req, res,next){
+	var username=req.body.username,
+		password=req.body.password;
+
+	// Authorized user
+	res.status(200).json({username: username, password: password});
+
+	// Unauthorized user
+	// res.status(401).json({message:'Unauthorized',username: username, password: password});
+
+	// Unknown Error
+	// res.status(400).json({username: username, password: password});
+});
+
 module.exports = router;
